a1d16405dc60b606b058d114908c9f72ae1be9fd
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:45:21 +0500

    Update Object.h

--- GEMS_V/Object.h
+++ GEMS_V/Object.h
@@ -3,19 +3,19 @@
 class Object
 {
 protected:
-	sf::Vector2f objectSize;
-	sf::Vector2f objectPosition;
+  sf::Vector2f objectSize;
+  sf::Vector2f objectPosition;
 
-	sf::RectangleShape shape;
+  sf::RectangleShape shape;
 
-	COLOR color_type;
-	sf::Color color;
+  COLOR color_type;
+  sf::Color color;
 
 public:
-	Object();
-	virtual ~Object() {}
-	COLOR GetColor();
-	void SetColor(COLOR color);
-	virtual void Draw(std::shared_ptr<sf::RenderWindow> window) = 0;
+  Object();
+  virtual ~Object() {}
+  COLOR GetColor();
+  void SetColor(COLOR color);
+  virtual void Draw(std::shared_ptr<sf::RenderWindow> window) = 0;
 };
 
