d010dd62a45711d557e72eb7edee36e13b74e229
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:43:44 +0500

    Update Game.h

--- GEMS_V/Game.h
+++ GEMS_V/Game.h
@@ -7,26 +7,26 @@
 
 class Game
 {
-	std::shared_ptr<PlayingField> field;
-	sf::Font font;
-	sf::Text text;
-	int scoreTotal;
-	std::string string;
-	STATE_GAME gameState;
-	sf::Vector2f windowSize;
-	std::shared_ptr <sf::RenderWindow> window;
-	std::shared_ptr <sf::Event> event;
+  std::shared_ptr<PlayingField> field;
+  sf::Font font;
+  sf::Text text;
+  int scoreTotal;
+  std::string string;
+  STATE_GAME gameState;
+  sf::Vector2f windowSize;
+  std::shared_ptr <sf::RenderWindow> window;
+  std::shared_ptr <sf::Event> event;
 public:
-	Game();
-	~Game() {}
-	void Run();
-	bool IsRunning();
-	STATE_GAME EndGame();
+  Game();
+  ~Game() {}
+  void Run();
+  bool IsRunning();
+  STATE_GAME EndGame();
 
-	void GameProcess();
+  void GameProcess();
 
-	void Draw();
-	void DrawBackground();
-	void DrawScore();
+  void Draw();
+  void DrawBackground();
+  void DrawScore();
 
 };
