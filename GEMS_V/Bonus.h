16a5279e979afa05f4bd33fe09b3f80ac732afe4
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:41:58 +0500

    Update Bonus.h

--- GEMS_V/Bonus.h
+++ GEMS_V/Bonus.h
@@ -5,23 +5,23 @@
 class Bonus : public Object
 {
 public:
-	Bonus() {}
-	virtual ~Bonus() {}
-	virtual void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow> window) = 0;
-	void Draw(std::shared_ptr<sf::RenderWindow> window) override {};
+  Bonus() {}
+  virtual ~Bonus() {}
+  virtual void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow> window) = 0;
+  void Draw(std::shared_ptr<sf::RenderWindow> window) override {};
 };
 
 class RepaintingBonus : public Bonus
 {
 public:
-	RepaintingBonus() {}
-	~RepaintingBonus() {}
-	void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow> window) override;
+  RepaintingBonus() {}
+  ~RepaintingBonus() {}
+  void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow> window) override;
 };
 class BombBonus : public Bonus
 {
 public:
-	BombBonus() {}
-	~BombBonus() {}
-	void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow>) override;
+  BombBonus() {}
+  ~BombBonus() {}
+  void Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow>) override;
 };
