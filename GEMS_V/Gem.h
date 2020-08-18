23e83c5ecb19c5951061c1d1305b34145bf55c15
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:44:17 +0500

    Update Gem.h

--- GEMS_V/Gem.h
+++ GEMS_V/Gem.h
@@ -4,10 +4,10 @@
 class Gem : public Object 
 {
 public:
-	Gem() {}
-	Gem(int x, int y);
-	~Gem() {}
-	void SetOutline(std::shared_ptr<sf::RenderWindow> window, sf::Color color);
-	void Draw(std::shared_ptr<sf::RenderWindow> window) override;
+  Gem() {}
+  Gem(int x, int y);
+  ~Gem() {}
+  void SetOutline(std::shared_ptr<sf::RenderWindow> window, sf::Color color);
+  void Draw(std::shared_ptr<sf::RenderWindow> window) override;
 };
 
