64e6071558b6550166e7afb282adf7a170a68cb9
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:44:40 +0500

    Update Object.cpp

--- GEMS_V/Object.cpp
+++ GEMS_V/Object.cpp
@@ -2,9 +2,9 @@
 Object::Object() : color_type((COLOR)0), color(sf::Color::White), objectSize(0.0, 0.0), objectPosition(0.0, 0.0), shape(objectSize) {}
 COLOR Object::GetColor()
 {
-	return color_type;
+  return color_type;
 }
 void Object::SetColor(COLOR color)
 {
-	color_type = (COLOR)color;
+  color_type = (COLOR)color;
 }
