7bc69bc7ab6bd05703d8578b148ca2e52c4c522c
nelli-nn <60840392+nelli-nn@users.noreply.github.com>
2020-08-18 20:20:48 +0500

    Update Bonus.cpp

--- GEMS_V/Bonus.cpp
+++ GEMS_V/Bonus.cpp
@@ -1,26 +1,25 @@
 #include "Bonus.h"
 void RepaintingBonus::Create(std::vector<std::vector<std::shared_ptr<Gem>>> gemMatrix, std::vector<std::vector <bool>> visited, std::shared_ptr<sf::RenderWindow> window) 
 {
-	int randIndexY[2];
-	randIndexY[0] = 2;
-	randIndexY[1] = -2;
-
-	for (int i = 0; i < X; i++)
-		for (int j = 0; j < Y; j++)
-		{
-			if (visited[i][j] == true)
-			{
-				if (rand() % 20 > 10)
-				{
-					int x = rand() % (2 - (-2)) - 2;
-					int y = rand() % (2 - (-2)) - 2;
-					if (abs(x) == 1 || x == 0)
-					{
-						y = randIndexY[rand() % 2];
-					}
-					int x_ = rand() % (2 - (-2)) - 2;
-					int y_ = rand() % (2 - (-2)) - 2;
-					if (abs(x_) == 1 || x_ == 0)
+  int randIndexY[2];
+  randIndexY[0] = 2;
+  randIndexY[1] = -2;
+  for (int i = 0; i < X; i++) 
+    for (int j = 0; j < Y; j++)
+    {
+       if (visited[i][j] == true)
+       {
+         if (rand() % 20 > 10)
+	 {
+	   int x = rand() % (2 - (-2)) - 2;
+           int y = rand() % (2 - (-2)) - 2;
+	   if (abs(x) == 1 || x == 0)
+	   {
+	     y = randIndexY[rand() % 2];
+	   }
+	   int x_ = rand() % (2 - (-2)) - 2;
+	   int y_ = rand() % (2 - (-2)) - 2;
+	   if (abs(x_) == 1 || x_ == 0)
 					{
 						y_ = randIndexY[rand() % 2];
 					}
